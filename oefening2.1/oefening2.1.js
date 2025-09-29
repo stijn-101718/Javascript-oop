24d279b142f971c9054dc8b06a5888c8ed9d3234
stijn <101718@glr.nl>
2025-09-29 12:20:03 +0000

    prima

--- oefening2.1/oefening2.1.js
+++ oefening2.1/oefening2.1.js
@@ -1,31 +1,37 @@
-class student  {
-    constructor(Studentennaam, studentnummer, klas, leerjaar, aantallesuren, aantalvakken, aantallesurenpervak) {
-    this.Studentennaam = Studentennaam;
-    this.studentnummer = studentnummer;
-    this.klas = klas;
-    this.leerjaar = leerjaar;
-    this.aantallesuren = aantallesuren;
-    this.aantalvakken = aantalvakken;
-    this.aantallesurenpervak = aantallesuren/aantalvakken;
-    
+
+const fs = require('fs');
+
+// Definieer eerst je class Student!
+class Student {
+    constructor(Studentennaam, studentnummer, klas, leerjaar, aantallesuren, aantalvakken) {
+        this.Studentennaam = Studentennaam;
+        this.studentnummer = studentnummer;
+        this.klas = klas;
+        this.leerjaar = leerjaar;
+        this.aantallesuren = Number(aantallesuren);
+        this.aantalvakken = Number(aantalvakken);
+        this.aantallesurenpervak = this.aantallesuren / this.aantalvakken;
     }
 
-    groet(){
-        console.log(`mijn naam is ${this.Studentennaam} mijn student nummer is ${this.studentnummer} ik zit in klas ${this.klas} en leerjaar ${this.leerjaar} ik heb ${this.aantallesuren} aantal lesuren en ${this.aantalvakken} aantal vakken en ik heb ${this.aantallesurenpervak} aantallesuren per vak`)
-
+    groet() {
+        console.log(`Mijn naam is ${this.Studentennaam}, mijn studentnummer is ${this.studentnummer}, ik zit in klas ${this.klas}, leerjaar ${this.leerjaar}, ik heb ${this.aantallesuren} lesuren, ${this.aantalvakken} vakken, dus ${this.aantallesurenpervak} lesuren per vak.`);
     }
 }
 
+// JSON inlezen
 
-let leerling1 = new student("Stijn", `d1f2`, 2, 101718, 32, 6, 32/6);
-let leerling2 = new student("Emma", `d1f2`, 1, 101713, 28, 7, 28/7);
-let leerling3 = new student("Lucas", `d1v2`, 3, 21321, 35, 5, 35/5);
-let leerling4 = new student("Sofia", `d1a2`, 2, 32131, 30, 6, 30/6);
-let leerling5 = new student("Noah", `d1c2`, 4, 42114, 40, 8, 40/8);
+const data = fs.readFileSync('oefening2.1/oefening2.1.json', 'utf-8');
+const studenten = JSON.parse(data);
 
-leerling1.groet();
-leerling2.groet();
-leerling3.groet();
-leerling4.groet();
-leerling5.groet();
-
+// Instances maken en groet aanroepen
+studenten.forEach(item => {
+    const leerling = new Student(
+        item.Studentennaam,
+        item.studentnummer,
+        item.klas,
+        item.leerjaar,
+        item.aantallesuren,
+        item.aantalvakken
+    );
+    leerling.groet();
+});
